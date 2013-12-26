71a8dd6e9441e1fd71f02f9904ed946ccd6d3ada
Lauri Junkkari <latenssi@gmail.com>
2013-12-26 18:16:49 +0200

    Initial commit

---
+++ arma2autojoin.py
@@ -0,0 +1,6 @@
+def main():
+	import subprocess
+	subprocess.call([r"C:\Program Files (x86)\Steam\steamapps\common\arma 2 operation arrowhead\ArmA2OA.exe", r"-high -cpuCount=4 -exThreads=7 -maxmem=2047 -noPause -nosplash -skipintro -world=empty"])
+
+if __name__ == "__main__":
+    main()
