004c1d35606a82b06deb4db2877504a80e2b1ba0
Lauri Junkkari <latenssi@gmail.com>
2013-12-26 22:53:48 +0200

    Working dayz epoch launcher

--- arma2autojoin.py
+++ arma2autojoin.py
@@ -1,17 +1,27 @@
 import subprocess
-import os
+import os 
 
 def main():
 	arma_path = r"C:\Program Files (x86)\Steam\steamapps\common\Arma 2"
-	arma_exe = r"arma2.exe"
 	oa_path = r"C:\Program Files (x86)\Steam\steamapps\common\arma 2 operation arrowhead"
-	oa_exe = r"ArmA2OA.exe"
-	mod = r"@DayZ_Epoch"
+	
+	os.chdir(oa_path)
+	
+	executable = r"expansion\beta\arma2oa.exe"
+	mod = r"-mod=%s;EXPANSION;ca" % arma_path
+	mod2 = r"-mod=Expansion\beta;Expansion\beta\Expansion;@dayz_epoch"
 	params = [r"-high", r"-cpuCount=4", r"-exThreads=7", r"-maxmem=2047", r"-noPause", r"-nosplash", r"-skipintro", r"-world=empty"]
 	network = [r"-connect=193.111.140.42", r"-port=2302"]
-	startup = [r"{0}".format(os.path.join(arma_path, arma_exe)), r"-mod={0}".format(os.path.join(oa_path, mod))]
+	
+	startup = []
+	startup.append(executable)
+	startup.append(mod)
+	startup.append(mod2)
 	startup.extend(params)
 	startup.extend(network)
+	
+	print "Starting DayZ Epoch.."
+	
 	subprocess.call(startup)
 
 if __name__ == "__main__":
