bfe1ae8a9d4d5f1b1fa0f71bb19a3c9c061dfefb
Lauri Junkkari <latenssi@gmail.com>
2013-12-27 22:04:27 +0200

    Some kind of configutarion capabilities

--- arma2autojoin.py
+++ arma2autojoin.py
@@ -2,31 +2,49 @@ import subprocess
 import os 
 import sys 
 import time
+import configparser
+import argparse
 from Server import Server
 
+CONF_FILE = "arma2autojoin.cfg"
 
 class Arma2AutoJoin:
 	
 	server_mon = None
 	
-	def __init__(self, host=None, port=None, mod = r"@dayz_epoch"):
+	def __init__(self, host, port, mod, arma_path, oa_path, cpucount=None, exthreads=None, maxmem=None):
 		self.host = host
 		self.port = port
 		self.mod = mod
-		if self.host and self.port:
-			self.server_mon = Server(host, port)
-			
-	def connect(self, host, port, mod):
-		arma_path = r"C:\Program Files (x86)\Steam\steamapps\common\Arma 2"
-		oa_path = r"C:\Program Files (x86)\Steam\steamapps\common\arma 2 operation arrowhead"
+		self.arma_path = arma_path
+		self.oa_path = oa_path
+		self.cpucount = cpucount
+		self.exthreads = exthreads
+		self.maxmem = maxmem
 		
-		os.chdir(oa_path)
-		
+	def connect(self):
+		os.chdir(self.oa_path)
 		exe_str = r"expansion\beta\arma2oa.exe"
-		mod_str = r"-mod=%s;EXPANSION;ca" % arma_path
-		mod2_str = r"-mod=Expansion\beta;Expansion\beta\Expansion;%s" % mod
-		params = [r"-high", r"-cpuCount=4", r"-exThreads=7", r"-maxmem=2047", r"-noPause", r"-nosplash", r"-skipintro", r"-world=empty"]
-		network = [r"-connect=%s" % host, r"-port=%d" % port]
+		mod_str = r"-mod=%s;EXPANSION;ca" % self.arma_path
+		mod2_str = r"-mod=Expansion\beta;Expansion\beta\Expansion;%s" % self.mod
+		params = [
+			r"-high", 
+			r"-noPause", 
+			r"-nosplash", 
+			r"-skipintro", 
+			r"-world=empty",
+		]
+		
+		if self.cpu_count:
+			params.append(r"-cpuCount=%d" % self.cpucount)
+		
+		if self.ext_hreads:
+			params.append(r"-exThreads=%d" % self.exthreads)
+			
+		if self.max_mem:
+			params.append(r"-maxmem=%d" % self.maxmem)
+			
+		network = [r"-connect=%s" % self.host, r"-port=%d" % self.port]
 		
 		startup = []
 		startup.append(exe_str)
@@ -48,9 +66,12 @@ class Arma2AutoJoin:
 			time.sleep(5)
 		
 		if auto_connect:
-			self.connect(host=self.host, port=self.port, mod=self.mod)
+			self.connect()
 			
 	def _query(self):
+		if not self.server_mon and (self.host and self.port):
+			self.server_mon = Server(self.host, self.port)
+			
 		while True:
 			try:
 				self.server_mon.query()
@@ -59,8 +80,31 @@ class Arma2AutoJoin:
 			else:
 				break
 		
-def main(host, port):
-	aaj = Arma2AutoJoin(host=host, port=port)
+def main(host=None, port=None):
+	config = configparser.RawConfigParser()
+	config.read(CONF_FILE)
+	
+	if host:
+		config.set('Server', 'host', host)
+		
+	if port:
+		config.set('Server', 'port', port)
+	
+	with open(CONF_FILE, 'w') as configfile:
+		config.write(configfile)
+	
+	
+	
+	aaj = Arma2AutoJoin(
+		config.get('Server', 'host'),
+		config.getint('Server', 'port'),
+		config.get('Settings', 'mod'),
+		config.get('Folders', 'arma_path'),
+		config.get('Folders', 'oa_path'),
+		cpucount=config.get('Settings', 'cpucount'),
+		exthreads=config.get('Settings', 'exThreads'),
+		maxmem=config.get('Settings', 'maxmem'),
+		)
 	aaj.monitor()
 	
 if __name__ == "__main__":
@@ -70,7 +114,10 @@ if __name__ == "__main__":
 		host = hostport[0]
 		port = int(hostport[1])
 	except:
-		print("Invalid arguments. Use 'python arma2autojoin.py host:port'") 
-		sys.exit(2)
-		
-	main(host, port)
+		port = None
+		try:
+			host = str(sys.argv[1])
+		except:
+			host = None
+
+	main(host=host, port=port)
