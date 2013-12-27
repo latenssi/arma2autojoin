4b86fa2707858daef8d8381ab6ed9d3e616acde9
Lauri Junkkari <latenssi@gmail.com>
2013-12-27 20:06:37 +0200

    More OO style design

--- arma2autojoin.py
+++ arma2autojoin.py
@@ -4,59 +4,64 @@ import sys
 import time
 from Server import Server
 
-def main(host, port):
 
-	mod = r"@dayz_epoch"
+class Arma2AutoJoin:
 	
-	arma_path = r"C:\Program Files (x86)\Steam\steamapps\common\Arma 2"
-	oa_path = r"C:\Program Files (x86)\Steam\steamapps\common\arma 2 operation arrowhead"
+	server_mon = None
 	
-	os.chdir(oa_path)
-	
-	exe_str = r"expansion\beta\arma2oa.exe"
-	mod_str = r"-mod=%s;EXPANSION;ca" % arma_path
-	mod2_str = r"-mod=Expansion\beta;Expansion\beta\Expansion;%s" % mod
-	params = [r"-high", r"-cpuCount=4", r"-exThreads=7", r"-maxmem=2047", r"-noPause", r"-nosplash", r"-skipintro", r"-world=empty"]
-	network = [r"-connect=%s" % host, r"-port=%d" % port]
-	
-	startup = []
-	startup.append(exe_str)
-	startup.append(mod_str)
-	startup.append(mod2_str)
-	startup.extend(params)
-	startup.extend(network)
-	
-	# Initialize the Server
-	s = Server(host, port)
-
-	print("\nConnecting to %s at port %d..\n" % (host, port))
-	while True:
-		try:
-			s.query()
-		except:
-			print("Server not respoding. Retrying..")
-		else:
-			print("Connected.")
-			break
+	def __init__(self, host=None, port=None, mod = r"@dayz_epoch"):
+		self.host = host
+		self.port = port
+		self.mod = mod
+		if self.host and self.port:
+			self.server_mon = Server(host, port)
 			
-	print("\n%s\n" % s.info['hostname'])
-	
-	while True:
-		max_players = int(s.info['maxplayers'])
-		cur_players = int(s.info['numplayers'])
-		print("Players: %d/%d" % (cur_players, max_players), end='\r')
-		if cur_players < max_players:
-			print("\nJoining the server..")
-			
-			# Start the game and connect to the server
-			subprocess.call(startup)
-			
-			break
-			
-		time.sleep(5)
-		s.query()
+	def connect(self, host, port, mod):
+		arma_path = r"C:\Program Files (x86)\Steam\steamapps\common\Arma 2"
+		oa_path = r"C:\Program Files (x86)\Steam\steamapps\common\arma 2 operation arrowhead"
 		
-	print("\nShutting down..")
+		os.chdir(oa_path)
+		
+		exe_str = r"expansion\beta\arma2oa.exe"
+		mod_str = r"-mod=%s;EXPANSION;ca" % arma_path
+		mod2_str = r"-mod=Expansion\beta;Expansion\beta\Expansion;%s" % mod
+		params = [r"-high", r"-cpuCount=4", r"-exThreads=7", r"-maxmem=2047", r"-noPause", r"-nosplash", r"-skipintro", r"-world=empty"]
+		network = [r"-connect=%s" % host, r"-port=%d" % port]
+		
+		startup = []
+		startup.append(exe_str)
+		startup.append(mod_str)
+		startup.append(mod2_str)
+		startup.extend(params)
+		startup.extend(network)
+		
+		subprocess.call(startup)
+	
+	def monitor(self, auto_connect=True):
+		while True:
+			self._query()
+			max_players = int(self.server_mon.info['maxplayers'])
+			cur_players = int(self.server_mon.info['numplayers'])
+			print("Players: %d/%d" % (cur_players, max_players), end='\r')
+			if auto_connect and cur_players < max_players:
+				break
+			time.sleep(5)
+		
+		if auto_connect:
+			self.connect(host=self.host, port=self.port, mod=self.mod)
+			
+	def _query(self):
+		while True:
+			try:
+				self.server_mon.query()
+			except:
+				print("Server not respoding. Retrying..")
+			else:
+				break
+		
+def main(host, port):
+	aaj = Arma2AutoJoin(host=host, port=port)
+	aaj.monitor()
 	
 if __name__ == "__main__":
 	try:
